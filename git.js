a16b8202b7a551919c21b87f60c8d1065a89456e
Your Name <abhisheklokhande02022gmail.com>
2022-10-23 20:14:48 +0530

    added prime and palindrome

---
+++ git.js
@@ -0,0 +1,27 @@
+//check prime or not:
+
+let num=11;
+let factor=10;
+
+for (i=0;i<=num;i++){
+if (factor%i==0){
+factor++;
+}
+}
+if (factor==2){
+console.log(num,"is prime");
+}else{
+console.log(num,"is not prime");
+}
+
+
+
+//check palindrome:
+
+
+str= "naman"
+let n= str.length
+for(i=0;i<=str.length-1;i++){
+if (str[0]==(n-1)&& str[1]==(n-2);
+}
+console.log(str,"is a palnidrome")
